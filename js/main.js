d0913932b57ecfea1c38d20b5d9df0a90d489bc7
Kristen Quinlan-Boyce <kqboyce@gmail.com>
2019-02-06 14:46:10 -0500

    rough draft

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const tubButton = document.getElementById("tubButton");
 const tubPic = document.getElementById("tubPic");
 
 tubButton.addEventListener('click', () => {
-  tubPic.style.transform = rotate(20deg);
+  tubPic.style.display="none";
 })
